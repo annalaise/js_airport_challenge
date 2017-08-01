410480560fc7cf67a64f153a76ca212778fb9e0d
antoinedoolaeghe <antoinedoolaeghe@gmail.com>
2017-08-01 17:19:50 +0100

    add rand behaviour test

--- dragonport_challenge/spec/CastleSpec.js
+++ dragonport_challenge/spec/CastleSpec.js
@@ -4,16 +4,37 @@ describe('Castle', function() {
   var dragon;
 
   beforeEach(function() {
-    castle = new Castle();
+    castle = new Castle(1);
     dragon = new Dragon();
-    console.log(castle.dragons)
+    spyOn(Math, 'random').and.returnValue(1);
     // dragon.land(castle)
   });
 
   describe('dragons', function() {
-    it('has dragons', function() {
+    it('has a dragon when landed a dragon', function() {
+      castle.princessCall()
       castle.land(dragon);
-      expect(castle.dragons()).toEqual([dragon]);
+      expect(castle.dragons()).toContain(dragon);
+    });
+
+    it("does not have a dragon when dragon fly off", function() {
+      castle.princessCall()
+      castle.land(dragon);
+      castle.flyOff(dragon)
+      expect(castle.dragons()).toEqual([]);
+    });
+
+    it("cannot land when exceeding the capacity", function() {
+      castle.princessCall()
+      castle.land(dragon);
+      castle.princessCall()
+      expect(castle.land(dragon)).toEqual('NOO Dragon! not needed today');
+    });
+  });
+
+  describe('princessCall', function() {
+    it('dragon cannot land without princessCall', function() {
+      expect(castle.land(dragon)).toEqual('NOO Dragon! not needed today');
     });
   });
 });
--- dragonport_challenge/src/Castle.js
+++ dragonport_challenge/src/Castle.js
@@ -1,5 +1,8 @@
-function Castle() {
+function Castle(capacity) {
   this._dragons = [];
+  this._dragonsCapacity = capacity;
+  this._call = false
+  this._CHANCE_OF_CALL = 0.5;
 }
 
 Castle.prototype.dragons =function() {
@@ -7,5 +10,26 @@ Castle.prototype.dragons =function() {
 }
 
 Castle.prototype.land = function(dragon) {
-  this._dragons.push(dragon);
+  if (this._call && (this._dragons.length < this._dragonsCapacity)) {
+    this._dragons.push(dragon);
+  } else {
+    return 'NOO Dragon! not needed today'
+  }
+}
+
+Castle.prototype.flyOff = function(dragon) {
+  var index = this._dragons.indexOf(dragon)
+  this._dragons.splice(index, 1);
+}
+
+Castle.prototype.rand = function() {
+  if (Math.random() > this._CHANCE_OF_CALL) {
+    return true
+  }
+}
+
+Castle.prototype.princessCall = function(dragon) {
+  if (this.rand()) {
+    this._call = true;
+  }
 }
