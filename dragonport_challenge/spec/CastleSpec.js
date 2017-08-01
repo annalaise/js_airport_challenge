15ca960179cfdd5fc9e962c303dc2513b3d33867
antoinedoolaeghe <antoinedoolaeghe@gmail.com>
2017-08-01 15:12:07 +0100

    second commit

---
+++ dragonport_challenge/spec/CastleSpec.js
@@ -0,0 +1,19 @@
+describe('Castle', function() {
+
+  var castle;
+  var dragon;
+
+  beforeEach(function() {
+    castle = new Castle();
+    dragon = new Dragon();
+    console.log(castle.dragons)
+    // dragon.land(castle)
+  });
+
+  describe('dragons', function() {
+    it('has dragons', function() {
+      castle.land(dragon)
+      expect(castle.dragons).toEqual([dragon]);
+    });
+  });
+});
---
+++ dragonport_challenge/src/Castle.js
@@ -0,0 +1,12 @@
+function Castle() {
+}
+
+Castle.prototype.dragons =function() {
+    this = [];
+}
+
+Castle.prototype.land = function(dragon) {
+  console.log(dragons);
+  console.log(Castle);
+  return dragons.push(dragon)
+}
